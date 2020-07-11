de968a984e6d04470f57f19f81fb90754b801022
chinglamchoi <ccl5a09@gmail.com>
2020-07-11 14:20:33 +0800

    foo

--- pretrain_ac.py
+++ pretrain_ac.py
@@ -572,6 +572,7 @@ def main_worker(gpu, ngpus_per_node, args):
   args.gpu = gpu
   print("args.gpu:", args.gpu)
   # problem: some code is only run once: solve soon!
+  #LAERT
   args.multiprocessing_distributed, args.distributed = True, True
   if args.multiprocessing_distributed and args.gpu != 0:
     def print_pass(*args):
